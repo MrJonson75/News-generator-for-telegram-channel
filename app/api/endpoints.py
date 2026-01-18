4c3688af82a65f4f8bbbcf82568250c57dbcdb3a
MrJonson <flashh@list.ru>
2026-01-18 19:17:50 +0300

    add endpoints.py edit endpoint, start fastapi

---
+++ app/api/endpoints.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.api.schemas import NewsItem, Post, Keyword, Source
+
+
+
+router = APIRouter(
+    prefix="/api",
+    tags=["api"],
+)
+
+
+
+@router.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+@router.get("/sources")
+async def get_sources():
+    return {"message": "ok"}
+
+
+@router.get("/keywords")
+async def get_keywords():
+    return {"message": "ok"}
+
+
+@router.get("/posts", response_model=List[Post], status_code=status.HTTP_200_OK)
+async def get_posts() -> List[NewsItem]:
+    pass
+
+
+
+@router.post("/generate")
+async def generate_post(news_item: NewsItem):
+    return {"message": "ok"}
+
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from app.config import settings
-
+from app.api.endpoints import router as endpoints_router
 
 
 app = FastAPI(
@@ -15,6 +15,7 @@ app = FastAPI(
     }
 )
 
+app.include_router(endpoints_router)
 
 
 @app.get("/")
@@ -24,4 +25,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
