2153ab2ea296e5e1f78dd966c3681e4e4bfd5c99
MrJonson <flashh@list.ru>
2026-01-20 16:48:38 +0300

    Finished the rbk.ru parser

--- app/news_parser/parser_rbk.py
+++ app/news_parser/parser_rbk.py
@@ -24,6 +24,7 @@ async def parse_news_rbk_site() -> List[Dict]:
     ]
     """
     url = settings.rbc_url
+
     html = await fetch_html(url)
 
     if not html:
@@ -35,56 +36,53 @@ async def parse_news_rbk_site() -> List[Dict]:
     soup = BeautifulSoup(html, "html.parser")
     news_items: List[Dict] = []
 
-    articles = soup.select("l-row js-load-container")
+    main_content = soup.select(".l-col-main")
+    articles = main_content[0].find_all("div", class_="item__wrap l-col-center")
     logger.info(f"Найдено статей: {len(articles)}")
-
     for item in articles:
         try:
-            print(item)
-            print()
             # --- Заголовок и ссылка ---
-    #         title_tag = item.find("a", class_="tm-title__link")
-    #         if not title_tag:
-    #             continue
-    #
-    #         title = title_tag.get_text(strip=True)
-    #         href = title_tag.get("href")
-    #
-    #         if not title or not href or "/ru/news" not in href:
-    #             continue
-    #
-    #         url_full = "https://habr.com" + href
+            title_tag = item.find("a", {"class": "item__link rm-cm-item-link js-rm-central-column-item-link"})
+            if not title_tag:
+                continue
 
-            # # --- Краткое описание ---
-            # summary_tag = item.find("div", class_="article-formatted-body")
-            # if not summary_tag:
-            #     continue
-            #
-            # summary = summary_tag.get_text(strip=True)
-            # if not summary:
-            #     continue
-            #
-            # # --- Дата публикации ---
-            # time_tag = item.find("time")
-            # published_at = time_tag.get("datetime") if time_tag else None
-            #
-            # # --- Ключевые слова (хабы) ---
-            # keywords: List[str] = []
-            # for key in item.select(".tm-publication-hub__link-container"):
-            #     keyword = key.get_text(strip=True).replace("*", "")
-            #     if keyword:
-            #         keywords.append(keyword.lower())
-            #
-            # news_items.append(
-            #     {
-            #         "title": title,
-            #         "url": url_full,
-            #         "summary": summary,
-            #         "source": "habr.com",
-            #         "published_at": published_at,
-            #         "keywords": keywords,
-            #     }
-            # )
+            title = title_tag.get_text(strip=True)
+            href = title_tag.get("href")
+            if not href or not title:
+                continue
+            html_article = await fetch_html(href)
+            if not html_article:
+                continue
+            # --- Краткое описание ---
+            sample_soup = BeautifulSoup(html_article, "html.parser")
+            summary_tag = sample_soup.find("div", {"class": "l-col-center-590 article__content"})
+            summary = summary_tag.find(
+                "div",
+                {"class": "article__text article__text_free"}
+                ).get_text(strip=True)[:400] + "..."
+            if not summary:
+                continue
+
+            # --- Дата публикации ---
+            time_tag = summary_tag.find("time")
+            published_at = time_tag.get("datetime") if time_tag else None
+
+            # --- Ключевые слова (хабы) ---
+            keywords: List[str] = []
+            for key in summary_tag.find_all("a", {"class": "article__tags__item"}):
+                keywords.append(key.get_text(strip=True))
+
+            # --- Добавляем статью в список ---
+            news_items.append(
+                {
+                    "title": title,
+                    "url": href,
+                    "summary": summary,
+                    "source": "rbc.ru",
+                    "published_at": published_at,
+                    "keywords": keywords,
+                }
+            )
 
         except Exception as e:
             logger.exception("❌ Ошибка при разборе статьи Habr")
@@ -97,8 +95,8 @@ async def parse_news_rbk_site() -> List[Dict]:
 # Тестовый запуск
 async def main():
     news = await parse_news_rbk_site()
-    # for item in news:
-    #     print(item)
+    for item in news:
+        print(item)
 
 
 if __name__ == "__main__":
